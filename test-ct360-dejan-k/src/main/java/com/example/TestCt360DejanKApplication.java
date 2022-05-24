809c1dc9754b93d665cbf64dee483145d4c6100b
Dejan Kulusic <dejan_kula@yahoo.com>
2022-05-24 16:23:33 +0200

    Third commit

--- test-ct360-dejan-k/src/main/java/com/example/TestCt360DejanKApplication.java
+++ test-ct360-dejan-k/src/main/java/com/example/TestCt360DejanKApplication.java
@@ -1,6 +1,7 @@
 package com.example;
 
 
+import com.example.translation.Translate;
 import org.jetbrains.annotations.NotNull;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -9,6 +10,7 @@ import org.springframework.boot.web.client.RestTemplateBuilder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.client.RestTemplate;
 
+import static com.example.translation.Translate.prettify;
 
 
 @SpringBootApplication
@@ -21,6 +23,13 @@ public class TestCt360DejanKApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(TestCt360DejanKApplication.class, args);
+		try {
+			Translate translateRequest = new Translate();
+			String response = translateRequest.Post();
+			System.out.println(prettify(response));
+		} catch (Exception e) {
+			System.out.println(e);
+		}
 	}
 
 
